e5ddedb6eecde35538a76b2dadbd488c24668203
guillaume-paris <guillaume1.paris@epitech.eu>
2024-03-14 22:05:34 +0100

    [ADD] project images

--- components/Projects.vue
+++ components/Projects.vue
@@ -8,8 +8,8 @@ import { projects } from '@/data/projects';
     <div class="flex flex-col gap-4">
       <h1 class="text-4xl font-bold">Projects</h1>
       <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div v-for="project in projects" :key="project.title" class="bg-custom-black rounded-2xl text-white">
-          <nuxt-img :src="project.banner" alt="Project banner" class="project-banner" />
+        <div v-for="project in projects" :key="project.title" class="bg-custom-black rounded-2xl text-white overflow-hidden">
+          <nuxt-img :src="project.banner" alt="Project banner" class="project-banner object-cover w-full h-52" />
           <h3>{{ project.title }}</h3>
           <p>{{ project.description }}</p>
         </div>
--- data/projects.js
+++ data/projects.js
@@ -1,22 +1,22 @@
 export const projects = [
   {
-    title: 'Project 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.',
-    banner: ""
+    title: 'My first Portfolio',
+    description: 'My First Portfolio is the culmination of my initial foray into full-stack web application development. Leveraging React and TailwindCSS for the frontend and Node.js for the backend, this project served as a platform for honing my skills in these technologies. The primary objective was to delve into backend development with Node.js and explore domain deployment practices, including the acquisition of a domain name.',
+    banner: "/images/first_portfolio.png"
   },
   {
-    title: 'Project 2',
-    description: 'Sed sit amet sem turpis. Curabitur lobortis id lorem id bibendum. Ut id consectetur magna.',
-    banner: "images/default_project_banner.png"
+    title: 'Asset Manager',
+    description: 'Asset Manager was conceived during my third internship at Dennemeyer. It represents a full-stack web application employing Angular on the frontend and .NET Core 6 on the backend, deployed on Azure. The project aimed to construct a robust web application featuring user authentication, account management, and asset tracking functionalities, fostering my understanding of backend development and cloud deployment practices.',
+    banner: "images/asset_manager.png"
   },
   {
-    title: 'Project 3',
-    description: 'Quisque volutpat augue enim, pulvinar lobortis nibh lacinia at. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.',
-    banner: "/images/default_project_banner.png"
+    title: 'PyDash',
+    description: 'PyDash emerged as a passion project, driven by the ambition to reimagine the game Geometry Dash using Python and the Pygame library. While drawing inspiration from existing repositories, I embarked on rewriting the game from scratch, enhancing it with a custom menu system. My future aspirations for PyDash include the integration of reinforcement machine learning to develop an AI capable of mastering levels autonomously.',
+    banner: "/images/pydash.png"
   },
   {
-    title: 'Project 4',
-    description: 'Duis at consectetur lorem. Donec massa sapien, faucibus et molestie ac, convallis non lorem.',
-    banner: "/images/default_project_banner.png"
+    title: 'Snake Game',
+    description: 'Snake Game represents another endeavor undertaken for personal enjoyment. Building upon the Game Engine I developed during my second year at Epitech, I endeavored to create a visual interface for playing the classic Snake game. Implemented in C++, this project showcases my proficiency in game development and serves as a testament to my ongoing passion for exploring new technologies and creative pursuits.',
+    banner: "/images/snake_game.png"
   }
 ];
