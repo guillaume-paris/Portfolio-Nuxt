ef6043ffa8e68a80e23c32c1dcd61f3519fa8b00
Guillaume Paris <guillaume1.paris@epitech.eu>
2024-03-20 13:10:45 +0100

    [ADD] content of educartion and 1/2 experiences

--- components/Education.vue
+++ components/Education.vue
@@ -1,12 +1,26 @@
 <template>
   <section id="education" class="min-h-screen flex justify-center items-center">
-    <div>
+    <div class="flex flex-col gap-4 w-full">
       <h2 class="text-4xl font-bold">Education</h2>
-      <div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Sed sit amet sem turpis. Curabitur lobortis id lorem id bibendum. Ut id consectetur magna. Quisque volutpat augue enim, pulvinar lobortis nibh lacinia at. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis at consectetur lorem. Donec massa sapien, faucibus et molestie ac, convallis non lorem. Cras vel lacus in ipsum rutrum dapibus sit amet eu quam.</p>
-        <p>Quisque lacinia in ligula non consequat. Mauris ipsum nulla, aliquet quis dignissim in, eleifend in libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin volutpat, dolor ut auctor luctus, leo metus venenatis dolor, et mollis lorem ipsum sed risus. In sit amet felis malesuada, feugiat purus eget, varius mi. Proin eget mauris nisl. Curabitur sed lacinia eros, et adipiscing dui. Etiam fermentum convallis ligula, et feugiat tortor cursus in.</p>
-        <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla facilisi. Duis aliquet egestas purus in blandit. Curabitur vulputate, ligula lacinia scelerisque tempor, lacus lacus ornare ante, ac egestas est urna sit amet arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed molestie augue sit amet leo consequat posuere. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin vel ante a orci tempus eleifend ut et magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pharetra vel augue et venenatis.</p>
-        <p>Sed sit amet orci eget libero ullamcorper tincidunt. Morbi libero lectus, laoreet in volutpat eu, varius vel massa. In hac habitasse platea dictumst. Fusce euismod consequat ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget commodo orci. Integer varius neque vel libero adipiscing, et iaculis justo ornare. Etiam ante est, tempor ut posuere nec, venenatis vitae velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+      <div class="flex flex-col gap-4">
+        <span class="text-2xl font-bold">Epitech 2020 - 2025</span>
+        <ul class="flex flex-col">
+          <li class="text-lg">
+            <span>Learn of basic programming</span>
+            <li class="text-lg">C for the first year</li>
+            <li class="text-lg">C++ for the second</li>
+            <li class="text-lg">Javascript, python for the third</li>
+          </li>
+        </ul>
+        <span class="text-2xl font-bold">Hanzehogeschool 2023 - 2024</span>
+        <ul class="flex flex-col">
+          <li class="text-lg">
+            First semester: IT Management
+          </li>
+          <li class="text-lg">
+            Second semester: AI & your future profession
+          </li>
+        </ul>
       </div>
     </div>
   </section>
--- components/Experiences.vue
+++ components/Experiences.vue
@@ -1,12 +1,22 @@
 <template>
     <section id="experiences" class="min-h-screen flex justify-center items-center">
-      <div>
+      <div class="flex flex-col gap-4 w-full">
         <h2 class="text-4xl font-bold">Experiences</h2>
-        <div>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Sed sit amet sem turpis. Curabitur lobortis id lorem id bibendum. Ut id consectetur magna. Quisque volutpat augue enim, pulvinar lobortis nibh lacinia at. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis at consectetur lorem. Donec massa sapien, faucibus et molestie ac, convallis non lorem. Cras vel lacus in ipsum rutrum dapibus sit amet eu quam.</p>
-          <p>Quisque lacinia in ligula non consequat. Mauris ipsum nulla, aliquet quis dignissim in, eleifend in libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin volutpat, dolor ut auctor luctus, leo metus venenatis dolor, et mollis lorem ipsum sed risus. In sit amet felis malesuada, feugiat purus eget, varius mi. Proin eget mauris nisl. Curabitur sed lacinia eros, et adipiscing dui. Etiam fermentum convallis ligula, et feugiat tortor cursus in.</p>
-          <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla facilisi. Duis aliquet egestas purus in blandit. Curabitur vulputate, ligula lacinia scelerisque tempor, lacus lacus ornare ante, ac egestas est urna sit amet arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed molestie augue sit amet leo consequat posuere. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin vel ante a orci tempus eleifend ut et magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pharetra vel augue et venenatis.</p>
-          <p>Sed sit amet orci eget libero ullamcorper tincidunt. Morbi libero lectus, laoreet in volutpat eu, varius vel massa. In hac habitasse platea dictumst. Fusce euismod consequat ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget commodo orci. Integer varius neque vel libero adipiscing, et iaculis justo ornare. Etiam ante est, tempor ut posuere nec, venenatis vitae velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <div class="flex flex-col gap-4">
+          <span class="text-2xl font-bold">First year</span>
+          <span class="text-lg italic">no experience yet</span>
+          <span class="text-2xl font-bold">Second year</span>
+          <ul class="flex flex-col">
+            <li class="text-lg">
+              6 months fulltime internship at PWC (full description)
+            </li>
+          </ul>
+          <span class="text-2xl font-bold">Second year</span>
+          <ul class="flex flex-col">
+            <li class="text-lg">
+              6 months fulltime internship at PWC (full description)
+            </li>
+          </ul>
         </div>
       </div>
     </section>
